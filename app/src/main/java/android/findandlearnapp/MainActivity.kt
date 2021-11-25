1a221b9a3036d2d0b2d10472972dab2b45afe714
Andrew <andrespin@mail.ru>
2021-11-25 15:34:41 +0300

    Fragment added

--- app/src/main/java/android/findandlearnapp/MainActivity.kt
+++ app/src/main/java/android/findandlearnapp/MainActivity.kt
@@ -2,10 +2,13 @@ package android.findandlearnapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.nav_movies_host_fragment)
+        navController.setGraph(R.navigation.dictionary_graph, intent.extras)
     }
 }
--- app/src/main/java/android/findandlearnapp/dictionary/DictionaryViewModel.kt
+++ app/src/main/java/android/findandlearnapp/dictionary/DictionaryViewModel.kt
@@ -1,4 +1,4 @@
 package android.findandlearnapp.dictionary
 
-class Frag {
+class DictionaryViewModel {
 }
