d87bb12b418b5ca54be70e9ec6d16f0068f07c0e
Andrew <andrespin@mail.ru>
2021-11-26 16:15:15 +0300

    Dict works

--- app/src/main/java/android/findandlearnapp/dictionary/DictionaryFragment.kt
+++ app/src/main/java/android/findandlearnapp/dictionary/DictionaryFragment.kt
@@ -7,19 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.findandlearnapp.R
+import android.findandlearnapp.base.BaseFragment
 import android.findandlearnapp.databinding.FragmentDictionaryBinding
 import android.findandlearnapp.dictionary.adapter.DictionaryAdapter
+import android.findandlearnapp.dictionary.data.AppState
+import android.util.Log
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 
-class DictionaryFragment : Fragment() {
+class DictionaryFragment : BaseFragment() {
 
     private lateinit var binding: FragmentDictionaryBinding
 
     private lateinit var viewModel: DictionaryViewModel
 
     private val adapter: DictionaryAdapter by lazy { DictionaryAdapter() }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,10 +35,58 @@ class DictionaryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
         viewModel = ViewModelProvider(this).get(DictionaryViewModel::class.java).apply {
             App.instance.appComponent.inject(this)
         }
 
+        viewModel.liveDataForViewToObserve.observe(viewLifecycleOwner, { renderData(it) })
+        initListeners()
+    }
+
+    private fun renderData(appState: AppState) {
+
+        when (appState) {
+            is AppState.Success -> {
+                showViewWorking()
+                val data = appState.data
+                println(data)
+                try {
+                    //   binding.txtWord.text = data[0].textOrig
+                } catch (e: Exception) {
+                    Log.e("Exception", e.message!!)
+                }
+                binding.txtWord.text = data.textOrig
+                binding.txtPhonetics.text = data.txtPhonetics
+                adapter.setData(data.wordDescriptions)
+            }
+
+            is AppState.Loading -> {
+                showViewLoading()
+            }
+
+            is AppState.Error -> {
+                showViewWorking()
+                showAlertDialog("Error", appState.error.message)
+            }
+        }
+    }
+
+
+    private fun showViewWorking() {
+        binding.progressBarRound.visibility = View.GONE
+    }
+
+    private fun showViewLoading() {
+        binding.progressBarRound.visibility = View.VISIBLE
+    }
+
+    private fun initViews() {
+        binding.rvTranslation.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTranslation.adapter = adapter
+    }
+
+    private fun initListeners() {
         binding.searchViewFindWord.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 viewModel.translateTheWord(query ?: "")
@@ -47,11 +98,7 @@ class DictionaryFragment : Fragment() {
                 return false
             }
         })
-
-
     }
-
-
 
     companion object {
 
